3dd0e3e2761dc24009127b3d8eedabe9ebb3280d
qq85715512 <ustb_caiwm@163.com>
2014-03-02 12:47:47 +0800

    1.添加了findByUserName和loginStaff

--- src/main/java/dao/StaffDao.java
+++ src/main/java/dao/StaffDao.java
@@ -1,9 +1,14 @@
 package dao;
 
+import java.util.List;
+
 import pojos.Staff;
 
 public interface StaffDao extends GenericDao<Staff>{
 
 	public Staff getStaffByUserNamePassword(String userName,String password);
 	
+	public Staff loginStaff(String userName);
+	
+	public List<Staff> findByUserName(String userName);
 }
--- src/main/java/dao/hibernate/StaffDaoHib.java
+++ src/main/java/dao/hibernate/StaffDaoHib.java
@@ -21,4 +21,25 @@ public class StaffDaoHib extends GenericDaoHib<Staff> implements StaffDao{
 		}
 		return null;
 	}
+
+	@Override
+	public Staff loginStaff(String userName) {
+		System.out.println(userName);
+		List<Staff> staffs=this.findByProperty("userName", userName);
+		System.out.println(staffs.size());
+		if(staffs.size()>0){
+			return staffs.get(0);
+		}else {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Staff> findByUserName(String userName) {
+		List<Staff> staffs=this.findByProperty("userName", userName);
+		if(staffs.size()>0){
+			return staffs;
+		}
+		return null;
+	}
 }
