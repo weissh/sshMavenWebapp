2c14edbc1bbe7121070c25f1e2d2573d33bc1996
aixiabaozi <yan@yan-PC>
2014-04-03 12:20:14 +0800

    /sshMavenWebapp/src/main/webapp/pages/system/user.js增加民族下拉框

--- src/main/webapp/pages/system/user.js
+++ src/main/webapp/pages/system/user.js
@@ -174,6 +174,71 @@ Ext.onReady(function(){
         ]
     });
     
+    //定义民族据源
+    var nationalities=new Ext.data.Store({
+    	autoLoad: true,
+        fieldLabel:30,
+        fields:['id','name'],
+        data:[
+	        {'id':'汉族','name':'汉族'},
+	        {'id':'满族','name':'满族'},
+	        {'id':'阿昌族','name':'阿昌族'},
+	        {'id':'白族','name':'白族'},
+	        {'id':'保安族 ','name':'保安族 '},
+	        {'id':'布朗族','name':'布朗族'},
+	        {'id':'布依族','name':'布依族'},
+	        {'id':'朝鲜族','name':'朝鲜族'},
+	        {'id':'达斡尔族','name':'达斡尔族'},
+	        {'id':'傣族','name':'傣族'},
+	        {'id':'德昂族','name':'德昂族'},
+	        {'id':'东乡族','name':'东乡族'},
+	        {'id':'侗族','name':'侗族'},
+	        {'id':'独龙族','name':'独龙族'},
+	        {'id':'俄罗斯族','name':'俄罗斯族'},
+	        {'id':'鄂伦春族','name':'鄂伦春族'},
+	        {'id':'鄂温克族','name':'鄂温克族'},
+	        {'id':'高山族 ','name':'高山族 '},
+	        {'id':'仡佬族 ','name':'仡佬族 '},
+	        {'id':'哈尼族 ','name':'哈尼族 '},
+	        {'id':'哈萨克族','name':'哈萨克族'},
+	        {'id':'赫哲族','name':'赫哲族'},
+	        {'id':'回族 ','name':'回族 '},
+	        {'id':'基诺族 ','name':'基诺族 '},
+	        {'id':'京族 ','name':'京族 '},
+	        {'id':'景颇族','name':'景颇族'},
+	        {'id':'柯尔克孜族 ','name':'柯尔克孜族 '},
+	        {'id':'拉祜族 ','name':'拉祜族 '},
+	        {'id':'黎族 ','name':'黎族 '},
+	        {'id':'傈僳族 ','name':'傈僳族 '},
+	        {'id':'珞巴族 ','name':'珞巴族 '},
+	        {'id':'毛南族','name':'毛南族'},
+	        {'id':'门巴族','name':'门巴族'},
+	        {'id':'蒙古族 ','name':'蒙古族 '},
+	        {'id':'苗族','name':'苗族'},
+	        {'id':'仫佬族 ','name':'仫佬族 '},
+	        {'id':'仫佬族 ','name':'仫佬族 '},
+	        {'id':'怒族','name':'怒族'},
+	        {'id':'普米族 ','name':'普米族 '},
+	        {'id':'羌族','name':'羌族'},
+	        {'id':'撒拉族','name':'撒拉族'},
+	        {'id':'畲族 ','name':'畲族 '},
+	        {'id':'水族','name':'水族'},
+	        {'id':'塔吉克族','name':'塔吉克族'},
+	        {'id':'塔塔尔族','name':'塔塔尔族'},
+	        {'id':'土家族','name':'土家族'},
+	        {'id':'土族','name':'土族'},
+	        {'id':'佤族 ','name':'佤族 '},
+	        {'id':'维吾尔族 ','name':'维吾尔族 '},
+	        {'id':'乌孜别克族','name':'乌孜别克族'},
+	        {'id':'锡伯族 ','name':'锡伯族 '},
+	        {'id':'瑶族','name':'瑶族'},
+	        {'id':'彝族','name':'彝族'},
+	        {'id':'裕固族','name':'裕固族'},
+	        {'id':'藏族 ','name':'藏族 '},
+	        {'id':'壮族 ','name':'壮族 '}
+        ]
+    });
+    
     //定义政治面貌数据源
     var politicalStatus=new Ext.data.Store({
     	autoLoad: true,
@@ -425,7 +490,16 @@ Ext.onReady(function(){
         	},
             {text: "年龄", width: 120, sortable: true, dataIndex: 'age',hidden:true},
             {text: "出生日期", width: 120, sortable: true, renderer: Ext.util.Format.dateRenderer('Y-m-d'),dataIndex: 'birthday',hidden:true},
-            {text: "民族", width: 120, sortable: true, dataIndex: 'nationality',hidden:true},
+            {text: "民族", 
+            width: 120, 
+            sortable: true, 
+            dataIndex: 'nationality',
+            renderer:function(value){//根据当前单元格的值，调用相应的store，并显示displayField；
+            		var index=nationalities.find('id',value);
+            		var record=nationalities.getAt(index);
+            		return getText(record);
+            	},
+            hidden:true},
             {
             	text: "政治面貌", 
             	width: 120, 
@@ -625,9 +699,18 @@ Ext.onReady(function(){
                     xtype:'textfield'
                 },
                 items: [
-                    {width:'33%',fieldLabel: '年龄',name: 'age',readOnly:true,allowBlank: false},
-                    {width:'33%',fieldLabel: '民族',name: 'nationality',value:'123',allowBlank: false}, 
-                    {
+	                    {width:'33%',fieldLabel: '年龄',name: 'age',readOnly:true,allowBlank: false},
+	                    {width:'33%',
+	                    fieldLabel: '民族',
+	                    xtype: 'combo',
+	                    name: 'nationality',
+	                    store:nationalities,
+                    	valueField:'id',
+                    	displayField:'name',
+	                    value:'汉族',
+	                    allowBlank: false
+	                    }, 
+	                    {
                     	width:'33%',
                     	fieldLabel: '政治面貌',
                     	xtype: 'combo',
